31d2e32426a5f32452a1fad4f342f38960310bcb
Fiz <56762330+fiza-11@users.noreply.github.com>
2023-12-28 02:07:08 +0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+// Function to greet the user
+function greetUser() {
+  alert("Welcome to Fiza's Escape Corner!");
+}
+
+// Function to toggle a dropdown menu
+function toggleDropdown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Function to change the background color
+function changeBackground() {
+  document.body.style.backgroundColor = "lightblue";
+}
