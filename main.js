e8fc9cba15b426d9dd8729b9712a509e41e0d167
Fiz <56762330+fiza-11@users.noreply.github.com>
2023-12-28 02:17:32 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+//Function to unblur the site
+function unblurSite() {
+  document.body.style.filter = "blur(0)";
+  document.querySelectorAll("section").forEach(section => {
+    section.style.filter = "blur(0)";
+  });
+}
+
 // Function to greet the user
 function greetUser() {
   alert("Welcome to Fiza's Escape Corner!");
