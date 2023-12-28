2dc8e4e185064dd0ac20987ce56a389471d75d23
Fiz <56762330+fiza-11@users.noreply.github.com>
2023-12-29 01:29:43 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+window.onload = () => {
+  const blurOverlay = document.querySelector(".blur-overlay");
+  const introText = document.querySelector(".intro-text");
+
+  // Show text first
+  introText.style.opacity = 1;
+
+  setTimeout(() => {
+    // Fade out both text and blur simultaneously
+    introText.style.opacity = 0;
+    blurOverlay.style.opacity = 0;
+
+    setTimeout(() => {
+      // Remove overflow: hidden to show unblurred content
+      document.querySelector(".landing-page").style.overflow = "visible";
+    }, 1500); /* Adjust delay as needed */
+  }, 2000); /* Duration for text visibility */
+};
+
 function greetUser() {
   alert("Welcome to Fiza's Escape Corner! If you're Fiza then you're the developer's favorite thing in the world, this space is for you. Have fun! And if not Fiza, leave immediately!");
 }
