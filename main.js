0ea453cb645a213af9a71c536200fd7c02471731
Fiz <56762330+fiza-11@users.noreply.github.com>
2023-12-29 18:14:12 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,3 @@
-window.onload = () => {
-  const blurOverlay = document.querySelector(".blur-overlay");
-  const introText = document.querySelector(".intro-text");
-
-  // Show text first
-  introText.style.opacity = 1;
-
-  setTimeout(() => {
-    // Fade out both text and blur simultaneously
-    introText.style.opacity = 0;
-    blurOverlay.style.opacity = 0;
-
-    setTimeout(() => {
-      // Remove overflow: hidden to show unblurred content
-      document.querySelector(".landing-page").style.overflow = "visible";
-    }, 1500); /* Adjust delay as needed */
-  }, 2000); /* Duration for text visibility */
-};
-
 function greetUser() {
   alert("Welcome to Fiza's Escape Corner! If you're Fiza then you're the developer's favorite thing in the world, this space is for you. Have fun! And if not Fiza, leave immediately!");
 }
@@ -26,7 +7,7 @@ function toggleDropdown() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-flag = true
+flag = false
 function changeBackground() {
   const bg = document.querySelector(".lofi-background");
   if (flag) {
@@ -35,16 +16,4 @@ function changeBackground() {
     bg.style.backgroundImage = "url(study-bg.jpg)";
   }
   flag = !flag; // Toggle the flag
-}
-
-function checkPassword() {
-  const enteredPassword = document.getElementById("passwordInput").value;
-  const correctPassword = "fizaorakzai"; // Replace with the actual password
-
-  if (enteredPassword === correctPassword) {
-    unblurSite();
-  } else {
-    // Handle incorrect password (e.g., display an error message)
-    alert("Incorrect password. Please try again.");
-  }
 }
